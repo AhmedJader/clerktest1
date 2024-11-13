5cac7f3b0487067bdb0e2605a90142eb5a850187
Ahmed <Ahmedjader837@gmail.com>
2024-11-13 04:55:51 -0500

    integrated the userboard and signout feature, under signedin auth route

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,20 @@
+import { SignedIn, UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 export default function HomePage() {
-  return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
-      <h2 className="text-center text-2xl font-extrabold">Welcome to the Home Page!</h2>
-      <p>You are successfully signed in.</p>
-    </div>
-  );
+    return (
+        <>
+            <div className='absolute top-4 right-4'>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+
+
+            <div className="w-full h-screen flex flex-col justify-center items-center">
+                <h2 className="text-center text-2xl font-extrabold">Welcome to the Home Page!</h2>
+                <p>You are successfully signed in.</p>
+            </div>
+        </>
+    );
 }
